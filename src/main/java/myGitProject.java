8aef98ba063a755ef1d8855fb7ffe8a4f97cc300
KobeGit <altenchen@aliyun.com>
2018-05-17 18:01:17 +0800

    This is caculation of c!

---
+++ src/main/java/myGitProject.java
@@ -0,0 +1,8 @@
+public class myGitProject {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 11;
+        int z = x * y;
+        System.out.println("c的计算值为：" + z);
+    }
+}
